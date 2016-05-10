22fe59b9a2a200887dfa002cb3736e112731d3b8
oakes <zsoakes@gmail.com>
2016-05-09 21:06:34 -0400

    Increment version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tag-soup "1.2.3-SNAPSHOT"
+(defproject tag-soup "1.2.3"
   :description "A library to parse code into a list of tags"
   :url "https://github.com/oakes/tag-soup"
   :license {:name "Public Domain"
