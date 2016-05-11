555efea7ab89ce0d9eeb9614056affd30d42e8c9
oakes <zsoakes@gmail.com>
2016-05-11 08:37:58 -0400

    Start 1.3.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tag-soup "1.3.0"
+(defproject tag-soup "1.3.1-SNAPSHOT"
   :description "A library to parse code into a list of tags"
   :url "https://github.com/oakes/tag-soup"
   :license {:name "Public Domain"
