e1d763594090b6842339e3ff649f3ae2927ddb28
oakes <zach@sekao.net>
2019-08-17 19:29:35 -0400

    Increment version number

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
-(defproject tag-soup "1.8.0-SNAPSHOT"
+(defproject tag-soup "1.7.1"
   :description "A library to parse code into a list of tags"
   :url "https://github.com/oakes/tag-soup"
   :license {:name "Public Domain"
             :url "http://unlicense.org/UNLICENSE"}
   :repositories [["clojars" {:url "https://clojars.org/repo"
-                             :sign-releases false}]]
-  :profiles {:dev {:main tag-soup.core}})
+                             :sign-releases false}]])
